0f8f6bd5db02f5ee7ac7c3f4fda2008e7ff9d215
Okey5759 <test>
2025-03-02 01:22:54 +0300

    test2

--- Program.cs
+++ Program.cs
@@ -19,9 +19,9 @@ builder.Services.AddDbContext<LearningDbContext>(
 
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) 
 {
     app.UseSwagger();
     app.UseSwaggerUI();
